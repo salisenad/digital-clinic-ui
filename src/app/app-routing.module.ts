626d54b0b607bf13664f9dc5ea10ac847c83aa01
Senad <senad.sali3@gmail.com>
2019-05-11 15:14:58 +0200

    Added components. Implementet routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { PatientComponent } from './patient/patient.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "patient", component: PatientComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PatientComponent } from './patient/patient.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
+    HeaderComponent, FooterComponent, SidebarComponent, DashboardComponent, PatientComponent,
+   
   ],
   imports: [
     BrowserModule,
